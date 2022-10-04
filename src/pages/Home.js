5bfc946037f8c5af0a22e68978f15035ae92a246
Laura Lachin <lauralachin@10-18-40-223.dynapool.wireless.nyu.edu>
2022-10-04 09:46:19 -0400

    Added weather API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { WEATHER_APP_API_KEY } from "../API_KEYS";
 
+// URL for accessing OpenWeatherAPI with Query Params
+const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=${WEATHER_APP_API_KEY}`;
+
 function Home() {
-    return (<div>
-        <h1>Weather App</h1>
-        <p>{WEATHER_APP_API_KEY}</p>
-    </div>);
+    // Value stored in state for weather data
+    const [weatherData, setWeatherData] = useState({});
+
+    useEffect(() => {
+    // Query OpenWeatherAPI for weather data
+    // make request to OpenWeather based on a city
+    axios
+        .get(openWeatherURL)
+        .then(function (response) {
+            setWeatherData(response.data)
+        })
+        .catch(function (error) {
+            console.warn(error);
+            setWeatherData({});
+        });
+    }, []);
+
+    // Display this weather data in our app
+    console.log("state value", weatherData);
+
+    return (
+        <div>
+            <h1>Weather App</h1>
+            <h2>{weatherData.name}</h2>
+            <p>Humidity: {weatherData.main && weatherData.main.humidity}</p>
+        </div>)
+    ;
 }
 
 export default Home;
